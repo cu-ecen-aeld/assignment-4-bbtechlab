4dfdd1b6ba0828e09f8463b2eaf6a92f186c0d44
bbtechlab <bbtechlab.vn@gmail.com>
2023-07-23 15:46:30 -0400

    assignment-4-part-1: multithreading and synchronization

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,6 +14,23 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* pData = (struct thread_data*)thread_param;
+    // wait for mutex lock
+    printf("wait for mutex lock\n");
+    usleep(pData->wait_to_obtain_ms * 1000);
+    // obtain mutex
+    printf("obtain mutex\n");
+    pthread_mutex_lock(pData->mutex);
+    // wait for mutex release
+    printf("wait for mutex release\n");
+    usleep(pData->wait_to_release_ms * 1000);
+    // release mutex
+    printf("release mutex\n");
+    pthread_mutex_unlock(pData->mutex);
+
+    printf("Update complete status\n");
+    pData->thread_complete_success = true;
+
     return thread_param;
 }
 
@@ -28,6 +45,27 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
+    // allocate memory for thread_data
+    struct thread_data* thread_data = malloc(sizeof(struct thread_data));
+    if (thread_data == NULL)
+    {
+        printf("Failed to allocate memory for thread_data\n");
+        goto exit;
+    }
+    // setup mutex and wait arguments
+    thread_data->mutex = mutex;
+    thread_data->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_data->wait_to_release_ms = wait_to_release_ms;
+    thread_data->thread_complete_success = false;
+    // pass thread_data to created thread
+    int threadId = pthread_create(thread, NULL, threadfunc, thread_data);
+    if (threadId != 0)
+    {
+        printf("Failed to created thread\n");
+        free(thread_data);
+        goto exit;
+    }
+exit:
     return false;
 }
 
